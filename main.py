416899a6407fcbc012a031af42da9b147f281c5d
norangai <norikongai@gmail.com>
2021-11-15 22:57:31 +0800

    add main

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 from fastapi import FastAPI
+import numpy as np
 import uvicorn
-
+from transformers import ElectraForSequenceClassification, ElectraTokenizer
+from pydantic import BaseModel
+import torch
+ 
 app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello Bigger Applications!"}
-
+ 
+class Data(BaseModel):
+    sentence: str
+ 
+@app.post("/sentiment")
+async def sentiment_classification(data:Data):
+   
+    model.eval()
+    pt_inputs = tokenizer(data.sentence, return_tensors="pt")
+    with torch.no_grad():
+        output = model(**pt_inputs)
+   
+    return {"message": labels[np.argmax(output.logits.cpu().numpy())]}
+ 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    tokenizer = ElectraTokenizer.from_pretrained('hfl/chinese-electra-180g-small-discriminator')
+    model = ElectraForSequenceClassification.from_pretrained("senti_clf_v1")
+    labels = ['cry', 'ok', 'smile']
+    uvicorn.run(app, host="0.0.0.0", port=8000,reload=True)
