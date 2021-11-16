4be5c91382f74bfa668a2d7a334918807117dc12
norangai <norikongai@gmail.com>
2021-11-16 22:57:18 +0800

    add dummy

--- main.py
+++ main.py
@@ -4,24 +4,28 @@ import uvicorn
 from transformers import ElectraForSequenceClassification, ElectraTokenizer
 from pydantic import BaseModel
 #import torch
- 
+
 app = FastAPI()
- 
+
 class Data(BaseModel):
     sentence: str
- 
+
+@app.get("/")
+async def test():
+    return {"message":"hello"}
+
 @app.post("/sentiment")
 async def sentiment_classification(data:Data):
-   
+
     model.eval()
     pt_inputs = tokenizer(data.sentence, return_tensors="pt")
     #with torch.no_grad():
     output = model(**pt_inputs)
-   
+
     return {"message": labels[np.argmax(output.logits.cpu().numpy())]}
- 
+
 if __name__ == "__main__":
     tokenizer = ElectraTokenizer.from_pretrained('hfl/chinese-electra-180g-small-discriminator')
     model = ElectraForSequenceClassification.from_pretrained("senti_clf_v1")
     labels = ['cry', 'ok', 'smile']
-    uvicorn.run(app, host="0.0.0.0", port=8000,reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
